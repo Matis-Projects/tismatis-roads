bc97015e406e7630e1adc2e2f98d7b9295b7eec0
Matis André <45696697+GameursProduction@users.noreply.github.com>
2022-09-19 18:09:24 +0200

    Update TismatisRoadsServer.java
    Fixed bug with server --> The server can't start with this mod because ModInitializer and not DedicatedServerModInitializer.

--- src/main/java/net/tismatis/tismatisroads/TismatisRoadsServer.java
+++ src/main/java/net/tismatis/tismatisroads/TismatisRoadsServer.java
@@ -1,18 +1,19 @@
 package net.tismatis.tismatisroads;
 
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static net.tismatis.tismatisroads.TismatisRoadsShared.InitializeElementsShared;
 
-public class TismatisRoadsServer implements ModInitializer {
+public class TismatisRoadsServer implements DedicatedServerModInitializer {
 
 	public static String MODID = TismatisRoadsShared.MODID;
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeServer() {
 		InitializeElementsShared();
 
 		LOGGER.info("[TismatisRoads-FABRIC] The server part has loaded!");
